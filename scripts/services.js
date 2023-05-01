62034c8fcf143cc2ab2624bcf3ed234752a88025
lllariogonzalez <lllariogonzalez@gmail.com>
2023-05-01 13:34:18 -0300

    Change render users style

--- scripts/services.js
+++ scripts/services.js
@@ -9,15 +9,17 @@ export async function getUsers() {
         users = data.map(user =>
             `<article class="card p-2" style="max-width: 700px;">
                 <div class="row col-lg-12 g-0 p-2">
-                    <div class="d-flex col-lg-3 col-6 gap-3 justify-content-center align-items-center">
+                    <div class="d-flex col-lg-3 col-12 gap-3 justify-content-center align-items-center">
                         <img src=${user.avatar} class="img-fluid col-6 rounded-circle w-100" alt="avatar">
                     </div>
                     <div class="col-lg-6 col-12">
-                        <div class="card-body">
-                            <h5 class="card-title">${user.name}</h5>
-                            <p class="card-text">dirección: ${user.adress}</p>
-                            <p class="card-text">${user.phoneNumber}</p>
-                            <p class="card-text">${user.email}</p>
+                        <div class="card-body d-flex flex-column align-items-center">
+                            <div>
+                                <h5 class="card-title">${user.name}</h5>
+                                <p class="card-text">🌎 ${user.adress}</p>
+                                <p class="card-text">📞 ${user.phoneNumber}</p>
+                                <p class="card-text">📨 ${user.email}</p>
+                            </div>
                         </div>
                     </div>
                     <div class="d-flex flex-column justify-content-center gap-2 col-lg-3 col-12">
